638c7bff76d097c3e29178266c9e9b29ce45beba
s1n7ax <srineshnisala@gmail.com>
2024-08-12 17:09:40 +0530

    feat: add extra language fonts

--- system/packages/fonts.nix
+++ system/packages/fonts.nix
@@ -2,6 +2,11 @@
 {
   fonts.fontconfig.enable = true;
   home.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-extra
+    noto-fonts-cjk-sans
+    noto-fonts-cjk-serif
+    noto-fonts-color-emoji
     (nerdfonts.override {
       fonts = [
         "FiraCode"
