08ae9399bc720d8be233ba6fc8dc4e9cd4cbcfb5
s1n7ax <srineshnisala@gmail.com>
2024-08-19 22:52:04 +0530

    chore: port some env vars and default configs from gituhb

--- system/applications/hyprland.nix
+++ system/applications/hyprland.nix
@@ -45,6 +45,16 @@ in
         "XCURSOR_SIZE,${cursor_size}"
         "HYPRCURSOR_THEME,${cursor_name}"
         "HYPRCURSOR_SIZE,${cursor_size}"
+
+        "XDG_CURRENT_DESKTOP,Hyprland"
+        "XDG_SESSION_TYPE,wayland"
+        "XDG_SESSION_DESKTOP,Hyprland"
+        "QT_QPA_PLATFORM,wayland;xcb"
+        "QT_QPA_PLATFORMTHEME,qt6ct"
+        "QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
+        "QT_AUTO_SCREEN_SCALE_FACTOR,1"
+        "MOZ_ENABLE_WAYLAND,1"
+        "GDK_SCALE,1"
       ];
 
       input = {
--- system/applications/styles.nix
+++ system/applications/styles.nix
@@ -13,6 +13,17 @@
     cursorTheme = {
       inherit (settings.cursor) name package size;
     };
+    gtk3.extraConfig = {
+      gtk-button-images = 0;
+      gtk-menu-images = 0;
+      gtk-enable-event-sounds = 1;
+      gtk-enable-input-feedback-sounds = 0;
+      gtk-xft-antialias = 1;
+      gtk-xft-hinting = 1;
+      gtk-xft-hintstyle = "hintfull";
+      gtk-xft-rgba = "rgb";
+      gtk-application-prefer-dark-theme = 1;
+    };
   };
 
   qt = {
